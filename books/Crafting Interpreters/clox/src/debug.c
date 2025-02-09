ee799c1874f0d7bea0b25b10d51304bd1f31ef7f
brettabamonte <abamonteb@gmail.com>
2025-02-09 17:02:58 -0500

    (add): chapter 29. added superclass support. single inheritance. added super expression

--- books/Crafting Interpreters/clox/src/debug.c
+++ books/Crafting Interpreters/clox/src/debug.c
@@ -81,16 +81,22 @@ int disassembleInstruction(Chunk *chunk, int offset) {
             return jumpInstruction("OP_LOOP", -1, chunk, offset);
         case OP_CLASS:
             return constantInstruction("OP_CLASS", chunk, offset);
+        case OP_INHERIT:
+            return simpleInstruction("OP_INHERIT", offset);
         case OP_METHOD:
             return constantInstruction("OP_METHOD", chunk, offset);
         case OP_GET_PROPERTY:
            return constantInstruction("OP_GET_PROPERTY", chunk, offset);
          case OP_SET_PROPERTY:
            return constantInstruction("OP_SET_PROPERTY", chunk, offset);
+         case OP_GET_SUPER:
+           return constantInstruction("OP_GET_SUPER", chunk, offset);
         case OP_CALL:
             return byteInstruction("OP_CALL", chunk, offset);
         case OP_INVOKE:
             return invokeInstruction("OP_INVOKE", chunk, offset);
+        case OP_SUPER_INVOKE:
+            return invokeInstruction("OP_SUPER_INVOKE", chunk, offset);
         case OP_CLOSURE:
             offset++;
             uint8_t constant = chunk->code[offset++];
